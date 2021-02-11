308e6cb6860bf4b8ba84829914a278803fd87be4
Antoine Bouët <antoine@famille-bouet.fr>
2021-02-11 16:23:43 +0100

    initial config

---
+++ tests/Hardware/PhoneControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Hardware;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PhoneControllerTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+}
